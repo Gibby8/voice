8a741a14196f9adfaed337e03f871d0a114f6a86
Gibby8 <87146390+Gibby8@users.noreply.github.com>
2022-07-06 19:30:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+    speak();
+    Webcam.attach(camera);
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}
+Webcam.set({
+    width:360,heigh:250
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
